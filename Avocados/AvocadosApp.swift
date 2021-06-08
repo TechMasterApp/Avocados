768c3290a07e67ebffa8faf11f25e4a8f6265853
Gaurav Bhasin <bhasingaurav@gmail.com>
2021-06-08 16:05:30 -0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Gaurav Bhasin on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
